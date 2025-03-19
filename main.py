a1da736c46cb1b4bd3926ba596a589dde3e15b72
TonightIwanttosleep <93720107+Napattnpp@users.noreply.github.com>
2025-03-19 19:21:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from ultralytics import YOLO
 
+# Load the YOLO8 model
+model = YOLO("yolo8n.pt")
+
+# Load the exported NCNN model (./best_ncnn_model)
+ncnn_model = YOLO("your_model_path")
+
+# Run inference
+results = ncnn_model("https://ultralytics.com/images/bus.jpg")
